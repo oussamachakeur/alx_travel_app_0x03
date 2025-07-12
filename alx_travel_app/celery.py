813b37e88e66971aadfc1d9518deb62400fde608
oussamachakeur <157352982+oussamachakeur@users.noreply.github.com>
2025-07-12 22:45:33 +0800

    Create celery.py

---
+++ alx_travel_app/celery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Celery app configuration for background task processing.
+"""
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_travel_app.settings')
+
+app = Celery('alx_travel_app')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
